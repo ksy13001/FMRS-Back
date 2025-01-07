6d9775378ec765f2d9d83c2d269958f3162e640a
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-01-07 16:28:24 +0900

    Initial commit

---
+++ fmrs/src/main/java/com/ksy/fmrs/FmrsApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.fmrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksy/fmrs/FmrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.fmrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
