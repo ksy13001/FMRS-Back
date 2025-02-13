0a16b7535669f7f421e495f97d10edf07ed75656
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-13 12:55:24 +0900

    feat: 컨트롤러 수정, 검색 결과 페이지 생성(구현x)

--- src/main/java/com/ksy/fmrs/controller/PlayerController.java
+++ src/main/java/com/ksy/fmrs/controller/PlayerController.java
@@ -3,6 +3,7 @@ package com.ksy.fmrs.controller;
 import com.ksy.fmrs.dto.PlayerDetailsResponseDto;
 import com.ksy.fmrs.dto.SearchPlayerCondition;
 import com.ksy.fmrs.dto.SearchPlayerResponseDto;
+import com.ksy.fmrs.dto.TeamPlayersResponseDto;
 import com.ksy.fmrs.service.PlayerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -22,17 +23,17 @@ public class PlayerController {
         return "player-detail";
     }
 
-//
-//    @GetMapping("/api/team/{teamId}/players")
-//    public TeamPlayersResponseDto getPlayers(@PathVariable Long teamId) {
-//        return playerService.getTeamPlayersByTeamId(teamId);
-//    }
-//
-//    @GetMapping("/api/search/simple-players")
-//    public SearchPlayerResponseDto searchPlayerByName(@RequestParam String name) {
-//        return playerService.searchPlayerByName(name);
-//    }
-//
+
+    @GetMapping("/api/team/{teamId}/players")
+    public TeamPlayersResponseDto getPlayers(@PathVariable Long teamId) {
+        return playerService.getTeamPlayersByTeamId(teamId);
+    }
+
+    @GetMapping("/api/search/simple-players")
+    public SearchPlayerResponseDto searchPlayerByName(@RequestParam String name) {
+        return playerService.searchPlayerByName(name);
+    }
+
     @GetMapping("/api/search/detail-players")
     public SearchPlayerResponseDto searchPlayerByDetailCondition(@RequestBody SearchPlayerCondition condition) {
         return playerService.searchPlayerByDetailCondition(condition);
