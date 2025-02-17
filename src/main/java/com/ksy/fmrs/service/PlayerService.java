f4301117d479d81baad66577f97b6a62dfa0ca1b
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-17 12:45:55 +0900

    refactor(playerService): footballApi 관련 메서드 분리

--- src/main/java/com/ksy/fmrs/service/PlayerService.java
+++ src/main/java/com/ksy/fmrs/service/PlayerService.java
@@ -17,17 +17,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class PlayerService {
-    @Value("${api-football.key}")
-    private String apiFootballKey;
-    @Value("${api-football.host}")
-    private String apiFootballHost;
 
-    private static final String TEAM_URL = "https://v3.football.api-sports.io/teams?";
-    private static final String PLAYER_STAT_URL = "https://v3.football.api-sports.io/players?";
-    private static final String NAME = "name=";
-    private static final String TEAM_ID = "team=";
-    private static final String AND = "&";
-    private final RestTemplate restTemplate;
     private final PlayerRepository playerRepository;
 
     /**
@@ -53,52 +43,6 @@ public class PlayerService {
         return Optional.ofNullable(player.getNation())
                 .map(Nation::getName)
                 .orElse(null);
-    }
-    /**
-     * 1. playerDetailDto 에서 playerName, teamName 가져옴
-     * 2. teamName 을 통해 api-football 에서 teamApiId 가져옴
-     * 3. playerName + teamApiId 를 통해 api-football 에서 playerApi + playerRealStat 가져옴
-     * */
-    public PlayerRealFootballStatDto getPlayerRealStat(String playerName, String teamName) {
-        String url = PLAYER_STAT_URL + NAME + playerName + AND + TEAM_ID + getTeamApiIdByTeamName(teamName);
-        ResponseEntity<PlayerStatisticsApiResponseDto> response = createAPIFootballRestClient()
-                .get()
-                .uri(url)
-                .retrieve()
-                .toEntity(PlayerStatisticsApiResponseDto.class);
-
-        return getPlayerRealFootballStatByStatistics(response.getBody());
-    }
-
-    // 출장경기수, 골, 어시스트, 평점
-    private PlayerRealFootballStatDto getPlayerRealFootballStatByStatistics(PlayerStatisticsApiResponseDto response) {
-        PlayerStatisticsApiResponseDto.StatisticDto stat = response.getResponse().get(0).getStatistics().get(0);
-        PlayerRealFootballStatDto playerRealStatDto = new PlayerRealFootballStatDto();
-        playerRealStatDto.setGamesPlayed(stat.getGames().getAppearences());
-        playerRealStatDto.setGoal(stat.getGoals().getTotal());
-        playerRealStatDto.setAssist(stat.getGoals().getAssists());
-        playerRealStatDto.setPk(stat.getPenalty().getScored());
-        playerRealStatDto.setRating(stat.getGames().getRating());
-        return playerRealStatDto;
-    }
-
-    private Integer getTeamApiIdByTeamName(String teamName){
-        String url = TEAM_URL+NAME+teamName;
-
-        ResponseEntity<TeamApiResponseDto> teamApiResponseDto = createAPIFootballRestClient()
-                .get()
-                .uri(url)
-                .retrieve()
-                .toEntity(TeamApiResponseDto.class);
-
-       return teamApiResponseDto.getBody().getResponse().get(0).getTeam().getId();
-    }
-
-    private RestClient createAPIFootballRestClient(){
-        return RestClient.builder()
-                .defaultHeader("X-RapidAPI-Key", apiFootballKey)
-                .defaultHeader("X-RapidAPI-Host", apiFootballHost)
-                .build();
     }
 
     /**
