2f3a71d487518701a643ea4777881ca464e33625
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-17 12:36:43 +0900

    feat: 실축 스탯 담을 dto 생성

---
+++ src/main/java/com/ksy/fmrs/dto/PlayerRealFootballStatDto.java
@@ -0,0 +1,12 @@
+package com.ksy.fmrs.dto;
+
+import lombok.Data;
+
+@Data
+public class PlayerRealFootballStatDto {
+    private int gamesPlayed;
+    private int goal;
+    private int pk;
+    private int assist;
+    private String rating;
+}
