48c5cb80dc8da444af4ff58023ee81cbf5e341b0
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-20 16:08:16 +0900

    feat(FootballApiService): Api-Football 검색 조건에 시즌 추가

--- src/main/java/com/ksy/fmrs/domain/enums/UrlEnum.java
+++ src/main/java/com/ksy/fmrs/domain/enums/UrlEnum.java
@@ -6,6 +6,8 @@ public enum UrlEnum {
     PARAM_NAME("name="),
     PARAM_SEARCH("search="),
     PARAM_TEAM("team="),
+    PARAM_SEASON("season="),
+    SEASON_2024_2025("2024"),
     AND("&");
 
     private final String value;
@@ -16,5 +18,21 @@ public enum UrlEnum {
 
     public String getValue() {
         return value;
+    }
+
+    // 선수 통계 API URL을 생성 (playerName, teamApiId, 시즌)
+    public static String buildPlayerStatUrl(String playerName, Integer teamApiId) {
+        return PLAYER_STAT_URL.getValue() +
+                PARAM_SEARCH.getValue() + playerName +
+                AND.getValue() +
+                PARAM_TEAM.getValue() + teamApiId +
+                AND.getValue() +
+                PARAM_SEASON.getValue() + SEASON_2024_2025.getValue();
+    }
+
+    // 팀 API URL을 생성 (teamName)
+    public static String buildTeamUrl(String teamName) {
+        return TEAM_URL.getValue() + PARAM_NAME.getValue()
+                + teamName;
     }
 }
