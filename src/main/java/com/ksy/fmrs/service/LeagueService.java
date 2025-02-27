6133fc549b6453958e75a01d1f4186f5f19e4f45
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-27 17:40:12 +0900

    feat(leagueService): league 저장 메서드 구현

--- src/main/java/com/ksy/fmrs/service/LeagueService.java
+++ src/main/java/com/ksy/fmrs/service/LeagueService.java
@@ -1,4 +1,30 @@
 package com.ksy.fmrs.service;
 
+import com.ksy.fmrs.domain.League;
+import com.ksy.fmrs.dto.LeagueDetailsDto;
+import com.ksy.fmrs.repository.LeagueRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class LeagueService {
+
+    private final LeagueRepository leagueRepository;
+
+    @Transactional
+    public void saveByLeagueDetails(LeagueDetailsDto leagueDetailsDto) {
+        League league = League.builder()
+                .leagueApiId(leagueDetailsDto.getLeagueApiId())
+                .name(leagueDetailsDto.getName())
+                .nation(leagueDetailsDto.getNationName())
+                .nationImageUrl(leagueDetailsDto.getNationImageUrl())
+                .currentSeason(leagueDetailsDto.getCurrentSeason())
+                .logoUrl(leagueDetailsDto.getLogoImageUrl())
+                .build();
+
+        leagueRepository.save(league);
+    }
+
 }
