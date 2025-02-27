eac10d61ed1e63d032f48ae8b69e896da94dc61b
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-27 17:56:55 +0900

    feat: team 상세 정보 담는 dto 생성

---
+++ src/main/java/com/ksy/fmrs/dto/TeamDetailsDto.java
@@ -0,0 +1,24 @@
+package com.ksy.fmrs.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TeamDetailsDto {
+    String teamName;
+    Integer teamApiId;
+    Integer leagueApiId;
+    String leagueName;
+    String leagueLogoImageUrl;
+    String nationName;
+    String nationLogoImageUrl;
+    Integer played;
+    Integer wins;
+    Integer losses;
+    Integer draws;
+    Integer goals;
+    Integer against;
+}
