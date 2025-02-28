24cf8bf416b93ed92fc2fe488086de7323e1eaae
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-02-28 16:38:29 +0900

    feat(TeamService): 팀 저장 기능 구현

--- src/main/java/com/ksy/fmrs/service/TeamService.java
+++ src/main/java/com/ksy/fmrs/service/TeamService.java
@@ -1,5 +1,9 @@
 package com.ksy.fmrs.service;
 
+import com.ksy.fmrs.domain.League;
+import com.ksy.fmrs.domain.Team;
+import com.ksy.fmrs.dto.TeamDetailsDto;
+import com.ksy.fmrs.repository.LeagueRepository;
 import com.ksy.fmrs.repository.Team.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,7 +12,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class TeamService {
     private final TeamRepository teamRepository;
+    private final LeagueRepository leagueRepository;
 
-    // 팀 소속 선수들 조회
+    public void saveByTeamDetails(TeamDetailsDto teamDetailsDto) {
+        Team team = Team.builder()
+                .name(teamDetailsDto.getTeamName())
+                .teamApiId(teamDetailsDto.getTeamApiId())
+                .logoUrl(teamDetailsDto.getLogoImageUrl())
+                .nationName(teamDetailsDto.getNationName())
+                .nationLogoImageUrl(teamDetailsDto.getNationLogoImageUrl())
+                .build();
+
+        League league = leagueRepository.findLeagueByLeagueApiId(teamDetailsDto.getLeagueApiId())
+                .orElseThrow(()-> new RuntimeException("League not found leagueApiId: " + teamDetailsDto.getLeagueApiId()));
+        team.updateLeague(league);
+        teamRepository.save(team);
+    }
 
 }
