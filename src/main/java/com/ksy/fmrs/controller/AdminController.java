00aa9954b320a9c55aea62b3711aab2a5290a353
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-04-30 05:10:31 +0900

    feat(AdminController): player 상세 조회 테스트 api 추가

--- src/main/java/com/ksy/fmrs/controller/AdminController.java
+++ src/main/java/com/ksy/fmrs/controller/AdminController.java
@@ -2,16 +2,15 @@ package com.ksy.fmrs.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ksy.fmrs.domain.player.Player;
+import com.ksy.fmrs.dto.PlayerOverviewDto;
 import com.ksy.fmrs.dto.player.PlayerDetailsDto;
 import com.ksy.fmrs.dto.player.PlayerStatDto;
 import com.ksy.fmrs.repository.Player.PlayerRawRepository;
 import com.ksy.fmrs.repository.Player.PlayerRepository;
-import com.ksy.fmrs.service.FootballApiService;
-import com.ksy.fmrs.service.InitializationService;
-import com.ksy.fmrs.service.PlayerService;
-import com.ksy.fmrs.service.SchedulerService;
+import com.ksy.fmrs.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -28,26 +27,16 @@ public class AdminController {
     private final FootballApiService footballApiService;
     private final InitializationService initializationService;
     private final SchedulerService  schedulerService;
+    private final PlayerFacadeService playerFacadeService;
     private static final int LAST_LEAGUE_ID = 1172;
     private static final int FIRST_LEAGUE_ID = 1;
     private final PlayerRepository playerRepository;
 
-    // 실축스탯 테스트용 api
     @ResponseBody
-    @GetMapping("/api/admin/player/{playerId}")
-    public PlayerStatDto getPlayerRealFootBallStatTest(@PathVariable Long playerId) {
-        PlayerDetailsDto playerDetailsResponseDto = playerService.getPlayerDetails(playerId);
-        return footballApiService.savePlayerRealStat(
-                playerDetailsResponseDto.getId(),
-                playerDetailsResponseDto.getPlayerApiId()
-        );
+    @GetMapping("/api/admin/players/{playerId}")
+    public PlayerOverviewDto getPlayerDetail(@PathVariable Long playerId) {
+        return playerFacadeService.getPlayerOverview(playerId);
     }
-
-//    @ResponseBody
-//    @PostMapping("/api/admin/insert/players/{fm_player}")  //fm_player
-//    public void insertPlayers(@PathVariable String fm_player) {
-//        initializationService.savePlayersFromFmPlayers(fm_player);
-//    }
 
     @ResponseBody
     @PostMapping("/api/admin/insert/player-data")  //fm_player
