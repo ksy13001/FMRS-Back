395fa9070c4228200ca2fe2fbfdb290e830d17c4
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-04-30 01:32:25 +0900

    feat(PlayerFacadeService): Player 관련 dto 합쳐서 반환하는 메서드 구현
    - Facade 패턴 적용: Player 정보, FmPlayer 정보, PlayerStat 정보 통합, 컨트롤러 간단하게 만들어줌

---
+++ src/main/java/com/ksy/fmrs/service/PlayerFacadeService.java
@@ -0,0 +1,24 @@
+package com.ksy.fmrs.service;
+
+import com.ksy.fmrs.dto.PlayerOverviewDto;
+import com.ksy.fmrs.dto.player.FmPlayerDetailsDto;
+import com.ksy.fmrs.dto.player.PlayerDetailsDto;
+import com.ksy.fmrs.dto.player.PlayerStatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PlayerFacadeService {
+    private final PlayerService playerService;
+    private final PlayerStatService playerStatService;
+
+    public PlayerOverviewDto getPlayerOverview(Long playerId) {
+        PlayerDetailsDto playerDetailsDto = playerService.getPlayerDetails(playerId);
+        PlayerStatDto playerStatDto = playerStatService.getPlayerStatByPlayerId(playerId);
+        FmPlayerDetailsDto fmPlayerDetailsDto = playerService.getFmPlayerDetails(playerId)
+                .orElse(null);
+
+        return new PlayerOverviewDto(playerDetailsDto, fmPlayerDetailsDto, playerStatDto);
+    }
+}
