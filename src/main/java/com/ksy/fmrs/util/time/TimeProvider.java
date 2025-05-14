b0e9be095baaa1c7aa4f7d3d5c566237035c10cb
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-05-14 17:56:01 +0900

    feat(SystemTimeProvider): localDate.now() 메서드 추가

--- src/main/java/com/ksy/fmrs/util/time/TimeProvider.java
+++ src/main/java/com/ksy/fmrs/util/time/TimeProvider.java
@@ -5,5 +5,6 @@ import java.time.LocalDateTime;
 
 public interface TimeProvider {
 
-    LocalDateTime getCurrentTime();
+    LocalDateTime getCurrentLocalDateTime();
+    LocalDate getCurrentLocalDate();
 }
