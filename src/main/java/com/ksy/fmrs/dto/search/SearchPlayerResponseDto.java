38a73368ed3eb0f01c91017fa740169919888aa5
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-05-14 22:44:04 +0900

    feat(SearchPlayerResponseDto): SearchPlayerResponseDto에 정적 펙토리 메소드 적용
    - page, slice 일때 구분

--- src/main/java/com/ksy/fmrs/dto/search/SearchPlayerResponseDto.java
+++ src/main/java/com/ksy/fmrs/dto/search/SearchPlayerResponseDto.java
@@ -4,11 +4,13 @@ import com.ksy.fmrs.domain.player.Player;
 import com.ksy.fmrs.dto.player.PlayerDetailsDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
 
+@Getter
 @Setter
 public class SearchPlayerResponseDto {
 
