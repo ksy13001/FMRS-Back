6f6a2472868ad0ca63f391f3071fe36a5ac467fc
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-06-12 20:49:52 +0900

    feat: Environment="FMRS_INIT_PLAYER_RAW=true" 로 서버 실행 시 초기 데이터 저장

---
+++ src/main/java/com/ksy/fmrs/config/InitializationConfig.java
@@ -0,0 +1,27 @@
+package com.ksy.fmrs.config;
+
+import com.ksy.fmrs.service.InitializationService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InitializationConfig {
+
+    /**
+     * 서버 restart 시 환경 변수 Environment="FMRS_INIT_PLAYER_RAW=true" 일 경우 서버 실행시 1번 실행됨
+     */
+    @Bean
+    @ConditionalOnProperty(name = "initial-data-insert", havingValue = "true")
+    public ApplicationRunner initializeDataInsert(InitializationService initializationService) {
+
+        return args -> {
+            initializationService.saveInitialLeague()
+                    .then(initializationService.saveInitialTeams())
+                    .then(initializationService.saveInitialPlayers())
+            .subscribe();
+        };
+    }
+
+}
