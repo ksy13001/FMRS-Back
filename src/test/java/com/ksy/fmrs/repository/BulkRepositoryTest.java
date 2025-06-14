4aa58457b13939b51ddc0aee22d7741e5e04fd59
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-06-14 23:11:55 +0900

    test(bulkRepositoryTest): 환경변수 추가
    - api-football.key, api-football.host

--- src/test/java/com/ksy/fmrs/repository/BulkRepositoryTest.java
+++ src/test/java/com/ksy/fmrs/repository/BulkRepositoryTest.java
@@ -15,39 +15,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {"api-football.key=test_key", "api-football.host=test_host"}
+)
 class BulkRepositoryTest {
 
-///
-//    @Test
-//    @DisplayName("Player List bulk insert 테스트")
-//    void bulkInsertPlayers_valid(){
-//        // given
-//        List<Player> players = new ArrayList<>();
-//        for(int i = 0; i < 1000; i++){
-//            Player player = createPlayer(i, "f", "l", "n1", "n1", LocalDate.now(),
-//                    180, 180, "image", MappingStatus.UNMAPPED);
-//            players.add(player);
-//        }
-//        // when
-//        bulkRepository.bulkInsertPlayers(players);
-//        // then
-//        TypedQuery<Player> result = entityManager.createQuery("SELECT p FROM Player p ", Player.class);
-//        Assertions.assertThat(result.getResultList()).hasSize(1000);
-//    }
-//
-//    private Player createPlayer(Integer playerApiId, String firstName, String lastName, String nationName, String nationLogo, LocalDate birth, int height, int weight, String imageUrl, MappingStatus mappingStatus){
-//        return Player.builder()
-//                .playerApiId(playerApiId)
-//                .firstName(firstName)
-//                .lastName(lastName)
-//                .nationName(nationName)
-//                .nationLogoUrl(nationLogo)
-//                .birth(birth)
-//                .height(height)
-//                .weight(weight)
-//                .imageUrl(imageUrl)
-//                .mappingStatus(mappingStatus)
-//                .build();
-//    }
+    @Autowired
+    private BulkRepository bulkRepository;
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    @DisplayName("Player List bulk insert 테스트")
+    void bulkInsertPlayers_valid(){
+        // given
+        List<Player> players = new ArrayList<>();
+        for(int i = 0; i < 1000; i++){
+            Player player = createPlayer(i, "f", "l", "n1", "n1", LocalDate.now(),
+                    180, 180, "image", MappingStatus.UNMAPPED);
+            players.add(player);
+        }
+        // when
+        bulkRepository.bulkInsertPlayers(players);
+        // then
+        TypedQuery<Player> result = entityManager.createQuery("SELECT p FROM Player p ", Player.class);
+        Assertions.assertThat(result.getResultList()).hasSize(1000);
+    }
+
+    private Player createPlayer(Integer playerApiId, String firstName, String lastName, String nationName, String nationLogo, LocalDate birth, int height, int weight, String imageUrl, MappingStatus mappingStatus){
+        return Player.builder()
+                .playerApiId(playerApiId)
+                .firstName(firstName)
+                .lastName(lastName)
+                .nationName(nationName)
+                .nationLogoUrl(nationLogo)
+                .birth(birth)
+                .height(height)
+                .weight(weight)
+                .imageUrl(imageUrl)
+                .mappingStatus(mappingStatus)
+                .build();
+    }
 }
