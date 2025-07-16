924edf3913f47af132323716894a39ef3d8e06bf
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-07-16 22:13:18 +0900

    fix: 헤더 모두 허용
    - logout, reissue permitAll

--- src/main/java/com/ksy/fmrs/config/SecurityConfig.java
+++ src/main/java/com/ksy/fmrs/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.ksy.fmrs.config;
 
 import com.ksy.fmrs.security.JwtFilter;
-import com.ksy.fmrs.security.JwtTokenProvider;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,9 +26,9 @@ import java.util.List;
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig{
+public class SecurityConfig {
 
-    private final JwtFilter  jwtFilter;
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
@@ -39,24 +36,24 @@ public class SecurityConfig{
                 // csrf 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 // cors 설저
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 // 세션 관리 정책: 세션 사용 안함
-                .sessionManagement(sm->{
-                    sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS);})
+                .sessionManagement(sm -> {
+                    sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
                 // 예외 처리 : 인증 실패 시 401 반환
-                .exceptionHandling(exception->
-                    exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 // 권한 없는 경우
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST,
-                                "/api/auth/signup","/api/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST,
-                                "/api/auth/logout", "api/auth/reissue").authenticated()
+                                "/api/auth/signup", "/api/auth/login","/api/auth/logout", "/api/auth/reissue").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/api/auth/status").authenticated()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET,
                                 "/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
@@ -65,7 +62,7 @@ public class SecurityConfig{
     }
 
     @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception{
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
@@ -79,13 +76,15 @@ public class SecurityConfig{
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
                 "https://www.fmrs.football/",
-                "http://localhost",
-                "http://localhost:3000/"
+                "http://localhost:*",
+                "https://localhost:*",
+                "http://localhost:3000",
+                "https://localhost:3000"
         ));
 //        config.setAllowedOriginPatterns(List.of("*"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
