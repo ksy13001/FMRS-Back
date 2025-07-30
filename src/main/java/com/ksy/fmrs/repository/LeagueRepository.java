44c6fcc07c9b3a97a2e4e7fd2bca785b86c3c393
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-07-30 16:17:17 +0900

    - 리그 초기 데이터 삽입 로직 보강

--- src/main/java/com/ksy/fmrs/repository/LeagueRepository.java
+++ src/main/java/com/ksy/fmrs/repository/LeagueRepository.java
@@ -2,12 +2,17 @@ package com.ksy.fmrs.repository;
 
 import com.ksy.fmrs.domain.League;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface LeagueRepository extends JpaRepository<League, Long> {
 
     Optional<League> findLeagueByLeagueApiId(Integer leagueApiId);
+    @Query("SELECT l.leagueApiId FROM League l")
+    Set<Integer> findAllLeagueApiIds();
 }
