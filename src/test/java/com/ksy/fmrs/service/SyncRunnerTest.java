39e1997ed097ffe87cfa580ab0594bea157c53db
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-09-20 19:06:04 +0900

    test: sync 테스트 추가

--- src/test/java/com/ksy/fmrs/service/SyncRunnerTest.java
+++ src/test/java/com/ksy/fmrs/service/SyncRunnerTest.java
@@ -1,11 +1,10 @@
 package com.ksy.fmrs.service;
 
 import com.ksy.fmrs.domain.League;
+import com.ksy.fmrs.domain.SyncJob;
 import com.ksy.fmrs.domain.enums.SyncType;
 import com.ksy.fmrs.dto.SyncReport;
 import com.ksy.fmrs.dto.apiFootball.ApiFootballLeague;
-import com.ksy.fmrs.repository.SyncFailedItemRepository;
-import com.ksy.fmrs.repository.SyncRunRepository;
 import com.ksy.fmrs.service.sync.SyncRecordService;
 import com.ksy.fmrs.service.sync.SyncRunner;
 import com.ksy.fmrs.service.sync.SyncStrategy;
@@ -13,19 +12,35 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class SyncRunnerTest {
 
     private SyncRunner syncRunner;
+    private SyncJob syncJob;
+    private static final LocalDateTime START_TIME = LocalDateTime
+            .of(2000, 8, 14, 0, 0, 0);
+    private static final String REQUEST_ERROR_MESSAGE = "requestSportsData exception";
+    @Mock
+    private SyncRecordService syncRecordService;
 
-    @Mock private SyncRecordService syncRecordService;
     @BeforeEach
     void setUp() {
         syncRunner = new SyncRunner(syncRecordService);
+        syncJob = SyncJob.started(SyncType.LEAGUE, START_TIME);
+
     }
 
     private static final int TEST_FIRST_LEAGUE_ID = 1;
@@ -33,9 +48,13 @@ class SyncRunnerTest {
 
 
     @Test
-    @DisplayName("Sync 중 예외 발생 시, failed ++ 하고 작업 재개")
+    @DisplayName("Sync 중 예외 발생 시, 실패한 아이템 저장하고 작업 재개")
     void Sync_With_Exception() {
-        // given && when
+        // given
+        given(syncRecordService.recordStarted(SyncType.LEAGUE))
+                .willReturn(syncJob);
+
+        // when
         SyncReport report = syncRunner.sync(
                 SyncType.LEAGUE,
                 IntStream.rangeClosed(TEST_FIRST_LEAGUE_ID, TEST_LAST_LEAGUE_ID).boxed().toList(),
@@ -45,13 +64,28 @@ class SyncRunnerTest {
         int total = TEST_LAST_LEAGUE_ID - TEST_FIRST_LEAGUE_ID + 1;
         int success = (int) IntStream.rangeClosed(TEST_FIRST_LEAGUE_ID, TEST_LAST_LEAGUE_ID).filter(i -> i % 3 != 0).count();
         checkReport(report, total, success, total - success);
+        IntStream.rangeClosed(TEST_FIRST_LEAGUE_ID, TEST_LAST_LEAGUE_ID)
+                .filter(i->i % 3 == 0)
+                .forEach(apiId->{
+                    verify(syncRecordService, times(1))
+                            .recordFailedItem(
+                                    SyncType.LEAGUE,
+                                    apiId,
+                                    REQUEST_ERROR_MESSAGE,
+                                    "RuntimeException",
+                                    syncJob);
+                }
+                );
     }
-
 
     @Test
     @DisplayName("입력된 Key 값이 없을 시, total=0")
     void Sync_With_Empty_Input() {
-        // given && when
+        // given
+        given(syncRecordService.recordStarted(SyncType.LEAGUE))
+                .willReturn(syncJob);
+
+        // when
         SyncReport report = syncRunner.sync(
                 SyncType.LEAGUE,
                 IntStream.rangeClosed(TEST_FIRST_LEAGUE_ID, -1).boxed().toList(),
@@ -60,24 +94,29 @@ class SyncRunnerTest {
 
         // then
         checkReport(report, 0, 0, 0);
-
+        verify(syncRecordService, never())
+                .recordFailedItem(any(), anyInt(), anyString(), anyString(), any());
     }
 
     @Test
-    @DisplayName("특정 키 작업중 예외 발생 시, 해당 키 작업 중지하고 다음 키 작업")
-    void requestSportsData_exception() throws Exception {
-        // given && when
-        failedStrategy failedStrategy = new failedStrategy();
+    @DisplayName("모든 작업 성공")
+    void requestSportsData_exception() {
+        // given
+        given(syncRecordService.recordStarted(SyncType.LEAGUE))
+                .willReturn(syncJob);
+        // when
+        allPassStrategy allPassStrategy = new allPassStrategy();
         SyncReport report = syncRunner.sync(
                 SyncType.LEAGUE,
                 IntStream.rangeClosed(TEST_FIRST_LEAGUE_ID, TEST_LAST_LEAGUE_ID).boxed().toList(),
-                failedStrategy
+                allPassStrategy
         );
 
         // then
         int total = TEST_LAST_LEAGUE_ID - TEST_FIRST_LEAGUE_ID + 1;
-        checkReport(report, total, 0, total);
-        Assertions.assertThat(failedStrategy.getNext()).isEqualTo(0);
+        checkReport(report, total, total, 0);
+        Assertions.assertThat(allPassStrategy.getNext()).isEqualTo(total);
+
     }
 
     private void checkReport(SyncReport report, int total, int success, int failed) {
@@ -89,7 +128,6 @@ class SyncRunnerTest {
                 .isEqualTo(failed);
     }
 
-
     static final class testLeagueStrategy implements SyncStrategy<Integer, ApiFootballLeague, League> {
         @Override
         public Integer getSyncApiId(Integer key) {return key;}
@@ -97,7 +135,7 @@ class SyncRunnerTest {
         @Override
         public List<ApiFootballLeague> requestSportsData(Integer key) {
             if (key % 3 == 0) {
-                throw new RuntimeException("test exception");
+                throw new RuntimeException(REQUEST_ERROR_MESSAGE);
             }
             return List.of();
         }
@@ -118,7 +156,7 @@ class SyncRunnerTest {
         }
     }
 
-    static final class failedStrategy implements SyncStrategy<Integer, ApiFootballLeague, League> {
+    static final class allPassStrategy implements SyncStrategy<Integer, ApiFootballLeague, League> {
         int next = 0;
 
         @Override
@@ -131,7 +169,6 @@ class SyncRunnerTest {
 
         @Override
         public void validate(List<ApiFootballLeague> dto) {
-            throw new RuntimeException("test exception");
         }
 
         @Override
