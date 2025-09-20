020bfa133a8980ca2bb3e04f6db48db17e3e453e
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-09-20 19:05:35 +0900

    refactor: SyncJobRepository 메서드 명 수정

--- src/main/java/com/ksy/fmrs/repository/SyncJobRepository.java
+++ src/main/java/com/ksy/fmrs/repository/SyncJobRepository.java
@@ -3,6 +3,5 @@ package com.ksy.fmrs.repository;
 import com.ksy.fmrs.domain.SyncJob;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SyncRunRepository extends JpaRepository<SyncJob, Long> {
-
+public interface SyncJobRepository extends JpaRepository<SyncJob, Long> {
 }
