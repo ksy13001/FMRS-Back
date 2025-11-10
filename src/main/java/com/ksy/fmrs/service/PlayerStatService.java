c8c0bff025076c70e3caf128ebf723cd77ee58e4
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-11-10 17:40:38 +0900

    refactor: PlayerStatService 에서 APiFootballClient 사용해서 데이터 가져오도록 리펙토링

--- src/main/java/com/ksy/fmrs/service/PlayerStatService.java
+++ src/main/java/com/ksy/fmrs/service/PlayerStatService.java
@@ -4,7 +4,6 @@ import com.ksy.fmrs.domain.League;
 import com.ksy.fmrs.domain.Team;
 import com.ksy.fmrs.domain.player.Player;
 import com.ksy.fmrs.domain.player.PlayerStat;
-import com.ksy.fmrs.dto.apiFootball.PlayerStatisticApiDto;
 import com.ksy.fmrs.dto.player.PlayerStatDto;
 import com.ksy.fmrs.mapper.PlayerStatMapper;
 import com.ksy.fmrs.repository.Player.PlayerRepository;
@@ -13,31 +12,24 @@ import com.ksy.fmrs.util.PlayerStatTtlProvider;
 import com.ksy.fmrs.util.time.TimeProvider;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PlayerStatService {
 
     private final PlayerRepository playerRepository;
     private final PlayerStatRepository playerStatRepository;
-    private final FootballApiService footballApiService;
+    private final ApiFootballClient apiFootballClient;
     private final PlayerStatMapper playerStatMapper;
     private final TimeProvider timeProvider;
     private final PlayerStatTtlProvider ttlProvider;
 
-    /**
-     * Player 통해서 playerStat 조회
-     * PlayerStat 이 존재 하고 수정 시각이 하루 미만일 경우 조회한 PlayerStat 사용
-     * 하루 이상일 경우 외부 api로 값 가져 오고 PlayerStat 업데이트
-     * PlayerStat이 존재 하지 않을 경우 외부 api로 값 가져 오기
-     * 팀 매핑된 경우에만 스탯 가져올수있음
-     */
     // 트랜잭션내의 외부 api 호출 분리하기
     @Transactional
     public Optional<PlayerStatDto> saveAndGetPlayerStat(Long playerId) {
@@ -54,16 +46,18 @@ public class PlayerStatService {
     }
 
     private Optional<PlayerStat> savePlayerStat(Player player) {
+
         Team team = Optional.ofNullable(player.getTeam())
                 .orElseThrow(()-> new EntityNotFoundException("Team not found"));
         League league = Optional.ofNullable(team.getLeague())
                 .orElseThrow(()-> new EntityNotFoundException("League not found"));
         PlayerStat ps = playerStatMapper.toEntity(
-                footballApiService.getPlayerStatByPlayerApiIdAndTeamApiIdAndLeagueApiId(
-                        player.getPlayerApiId(),
-                        team.getTeamApiId(),
+                apiFootballClient.requestPlayerStatistics(
                         league.getLeagueApiId(),
+                        team.getTeamApiId(),
+                        player.getPlayerApiId(),
                         league.getCurrentSeason()));
+
         if(ps == null) {
             return Optional.empty();
         }
