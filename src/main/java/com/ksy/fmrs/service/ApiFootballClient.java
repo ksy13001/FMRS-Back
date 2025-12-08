64625b27434c6de7658ef52d06a53ed27872f426
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2025-12-08 19:18:08 +0900

    feat(ApiFootballClient): Transfer apifootball 호출 메서드 구현

--- src/main/java/com/ksy/fmrs/service/ApiFootballClient.java
+++ src/main/java/com/ksy/fmrs/service/ApiFootballClient.java
@@ -1,9 +1,6 @@
 package com.ksy.fmrs.service;
 
-import com.ksy.fmrs.dto.apiFootball.ApiFootballLeague;
-import com.ksy.fmrs.dto.apiFootball.ApiFootballPlayersStatistics;
-import com.ksy.fmrs.dto.apiFootball.ApiFootballTeamsByLeague;
-import com.ksy.fmrs.dto.apiFootball.ApiFootballSquad;
+import com.ksy.fmrs.dto.apiFootball.*;
 
 public interface ApiFootballClient {
     ApiFootballLeague requestLeagueByApiId(Integer apiId);
@@ -11,4 +8,5 @@ public interface ApiFootballClient {
     ApiFootballPlayersStatistics requestPlayersByTeam(Integer teamApiId, int currentSeason, int page);
     ApiFootballSquad requestSquad(Integer teamApiId);
     ApiFootballPlayersStatistics requestPlayerStatistics( Integer leagueApiId, Integer teamApiId, Integer playerApiId, int currentSeason);
+    ApiFootballTransfers requestTransfers(Integer teamApiId);
 }
