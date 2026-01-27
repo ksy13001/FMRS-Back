3ce1a648880fcc672180218a9e48ecfb085ac1ea
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-01-27 20:23:15 +0900

    refactor: Player 상세 조회에 team/league fetch join 적용

--- src/main/java/com/ksy/fmrs/service/PlayerFacadeService.java
+++ src/main/java/com/ksy/fmrs/service/PlayerFacadeService.java
@@ -1,15 +1,18 @@
 package com.ksy.fmrs.service;
 
+import com.ksy.fmrs.domain.player.Player;
 import com.ksy.fmrs.dto.comment.CommentCountResponseDto;
 import com.ksy.fmrs.dto.player.PlayerOverviewDto;
 import com.ksy.fmrs.dto.player.FmPlayerDetailsDto;
 import com.ksy.fmrs.dto.player.PlayerDetailsDto;
 import com.ksy.fmrs.dto.player.PlayerStatDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PlayerFacadeService {
@@ -18,7 +21,10 @@ public class PlayerFacadeService {
     private final CommentService commentService;
 
     public PlayerOverviewDto getPlayerOverview(Long playerId) {
+        long start = System.currentTimeMillis();
         PlayerDetailsDto playerDetailsDto = playerService.getPlayerDetails(playerId);
+        long end = System.currentTimeMillis();
+        log.info("getPlayerOverview time : {}", end - start);
         PlayerStatDto playerStatDto = playerStatService.saveAndGetPlayerStat(playerId)
                 .orElse(null);
         List<FmPlayerDetailsDto> fmPlayerDetailsDto = playerService.findFmPlayerDetails(playerId)
