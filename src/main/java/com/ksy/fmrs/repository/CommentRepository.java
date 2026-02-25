0231c51445b98ebcb22bf241fed10b23a097e500
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-02-25 20:29:23 +0900

    feat(CommentRepository): comment count 조회시 Player JOIN 대신 WHERE 절 사용하도록 구현

--- src/main/java/com/ksy/fmrs/repository/CommentRepository.java
+++ src/main/java/com/ksy/fmrs/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.ksy.fmrs.domain.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,6 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByPlayerId(Long playerId, Pageable pageable);
+    @Query("SELECT COUNT(c.id) FROM Comment c WHERE c.player.id = :playerId")
     int countByPlayerId(Long playerId);
 }
