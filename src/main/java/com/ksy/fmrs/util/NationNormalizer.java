a0decd6f00fe6bfa0a1a2f4451cf27b8b2139ea9
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-03-19 18:42:44 +0900

    feat(nationNormalizer): 국가명 정규화 케이스 추가

--- src/main/java/com/ksy/fmrs/util/NationNormalizer.java
+++ src/main/java/com/ksy/fmrs/util/NationNormalizer.java
@@ -8,7 +8,9 @@ import java.util.Map;
 public class NationNormalizer{
     private static final Map<String, String> NATIONS = Map.of(
             "TURKEY", "TÜRKIYE",
+            "TÃ¼RKIYE", "TÜRKIYE",          // UTF-8 인코딩 깨진 케이스
             "CZECH REPUBLIC", "CZECHIA",
+            "FYR MACEDONIA", "NORTH MACEDONIA",
             "HONG KONG (CHINA PR)", "HONG KONG, CHINA",
             "KYRGYZSTAN", "KYRGYZ REPUBLIC",
             "ST. VINCENT / GRENADINES", "ST. VINCENT AND THE GRENADINES"
